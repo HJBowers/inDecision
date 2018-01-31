f4c7d9501a6ed3fa045a940c11ff4638c0edbcf9
HJBowers <HJBowers@gmail.com>
2018-01-30 17:49:07 -0800

    updated icons, package json

--- src/Decide.js
+++ src/Decide.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
+import Form from './Form.js'
+import logo from './Lyft.png';
 import './App.css';
 
-class Home extends Component {
+class Decide extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Decide</h1>
+          <h1 className="App-title">inDecision</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form></Form>
       </div>
     );
   }
 }
 
-export default Home;
+export default Decide;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
